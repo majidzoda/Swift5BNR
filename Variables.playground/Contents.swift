557debd2328ca5579475aa49d3ad918d027273ab
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-18 00:27:25 -0500

    String Interpolation p53-54

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -3,3 +3,6 @@ import UIKit
 let numberOfStopLights: Int = 4
 var population: Int
 population = 5422
+let townName: String = "Knowhere"
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStopLights) stoplights."
+print(townDescription)
