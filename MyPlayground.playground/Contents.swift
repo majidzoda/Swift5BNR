7e26c88e694dbf53147a3763b8356f624274383c
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-17 22:00:45 -0500

    Adding Challenge description

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,5 +6,8 @@ str += "!"
 print(str)
 
 // Chapter 1 Bronze Challenge p. 46
+/*
+ You learned about the String type and printing to the console using print(). Use your new playground to create a new instance of the String type. Set the value of this instance to be equal to your last name. Print its value to the console.*/
+
 var lastName = "Majidzoda"
 print(lastName)
