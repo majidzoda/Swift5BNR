2ec1a61b1f5c95479eb6c3063f368ce58e97a685
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-17 22:01:40 -0500

    Fixing indentation

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -7,7 +7,9 @@ print(str)
 
 // Chapter 1 Bronze Challenge p. 46
 /*
- You learned about the String type and printing to the console using print(). Use your new playground to create a new instance of the String type. Set the value of this instance to be equal to your last name. Print its value to the console.*/
+ You learned about the String type and printing to the console using print(). Use your new playground
+ to create a new instance of the String type. Set the value of this instance to be equal to your last
+ name. Print its value to the console.*/
 
 var lastName = "Majidzoda"
 print(lastName)
