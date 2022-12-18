5275cf2409c314b987652b72ffe1d0ae46856aef
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-18 00:34:44 -0500

    Bronze Challenge completed.

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,5 +4,15 @@ let numberOfStopLights: Int = 4
 var population: Int
 population = 5422
 let townName: String = "Knowhere"
-let townDescription = "\(townName) has a population of \(population) and \(numberOfStopLights) stoplights."
+var townDescription = "\(townName) has a population of \(population) and \(numberOfStopLights) stoplights"
+print(townDescription)
+
+/*
+Chapter 2 Bronze Challenge p.55
+Add a new variable to your playground representing Knowhere’s elevation. Which data type should you use?
+Give this variable a value and update townDescription to use this new information.
+*/
+
+let townElevation: Int = 2316
+townDescription += "And \(townElevation)' elevation."
 print(townDescription)
