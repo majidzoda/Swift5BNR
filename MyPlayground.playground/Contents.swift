229cf6c7a7b3841d57ab32b3073f65a959d1c344
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-17 21:56:03 -0500

    Chapter 1 Bronze Challenge p. 46

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,5 +2,9 @@ import UIKit
 
 var str = "Hello, playground"
 str += "!"
-g
+// print the string to the console
 print(str)
+
+// Chapter 1 Bronze Challenge p. 46
+var lastName = "Majidzoda"
+print(lastName)
