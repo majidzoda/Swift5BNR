26cf15aff8ad76849c4d5358103505a303b0b97b
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-19 12:17:05 -0500

    Creating Integer Instances p. 75-76

--- Numbers.playground/Contents.swift
+++ Numbers.playground/Contents.swift
@@ -11,3 +11,9 @@ print("The maximum UInt value is \(UInt.max).")
 print("The minimum UInt value is \(UInt.min).")
 print("The maximum value for a 32-bit unsigned integer is \(UInt32.max).")
 print("The minimum value for a 32-bit unsigned integer is \(UInt32.min).")
+
+let numbersOfPage: Int = 10
+let numberOfChapters = 3
+
+let numberOfPeople: UInt = 40
+let volumeAdjustment: Int32 = -1000
