2481ba3443f0258a338a78c9298a70cbddc19eb3
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-19 12:08:53 -0500

    Integers p.71-74

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+print("The maximum Int value is \(Int.max).")
+print("The minimum Int value is \(Int.min).")
+print("The maximum value for a 32-bit integer is \(Int32.max).")
+print("The minimum value for a 32-bit integer is \(Int32.min).")
+
+print("The maximum UInt value is \(UInt.max).")
+print("The minimum UInt value is \(UInt.min).")
+print("The maximum value for a 32-bit unsigned integer is \(UInt32.max).")
+print("The minimum value for a 32-bit unsigned integer is \(UInt32.min).")
