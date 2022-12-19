e8684746710f3c745629652c2b8c1af2a09884cd
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-19 12:56:09 -0500

    Floating-Point Numbers p. 84-86

--- Numbers.playground/Contents.swift
+++ Numbers.playground/Contents.swift
@@ -41,3 +41,19 @@ let z = y &+ 10
 let a: Int16 = 200
 let b: Int8 = 50
 let c = a + Int16(b)
+
+let d1 = 1.1
+let d2: Double = 1.1
+let f1: Float = 100.3
+
+10.0 + 11.4
+11.0 / 3.0
+
+if d1 == d2 {
+    print("d1 and d2 are the same!")
+}
+
+print("d1 + 0.1 is \(d1 + 0.1)")
+if d1 + 0.1 == 1.2 {
+    print("d1 + 0.1 is equal to 1.2")
+}
