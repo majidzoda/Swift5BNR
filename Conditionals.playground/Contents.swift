ed0c004df29e4b858a45445b58b421d3620112b2
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-19 11:08:45 -0500

    if/else p. 58-61

---
+++ Conditionals.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let population: Int = 5422
+let message: String
+let hasPostOffice: Bool = true
+
+if population < 10000 {
+    message = "\(population) is a small town!"
+} else {
+    message = "\(population) is pretty big!"
+}
+print(message)
+
+if !hasPostOffice {
+    print("Where do we buy stamps?")
+}
