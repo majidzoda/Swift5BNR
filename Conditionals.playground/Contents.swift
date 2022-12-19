845a745fc75550a7aa5e4158abf9010199155262
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-19 11:45:54 -0500

    Chapter 3 - Bronze Challenge completed.

--- Conditionals.playground/Contents.swift
+++ Conditionals.playground/Contents.swift
@@ -1,15 +1,22 @@
 import UIKit
 
-let population: Int = 5422
+let population: Int = 100000
 let message: String
 let hasPostOffice: Bool = true
 
+/*
+Chapter 3 Bronze Challenge p.69
+Add an additional else if statement to the town-sizing code to see if your town’s population is very
+large. Choose your own population thresholds. Set the message variable accordingly.
+*/
 if population < 10000 {
     message = "\(population) is a small town!"
 } else if population >= 10000 && population < 50000{
         message = "\(population) is a medium town!"
+} else if population >= 50000 && population < 100000{
+        message = "\(population) is a medium big!"
 } else {
-    message = "\(population) is pretty big!"
+    message = "\(population) is very large!"
 }
 print(message)
 
