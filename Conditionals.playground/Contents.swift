2d37a16d69a25fea85c4c45478686ec21971c117
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-19 11:32:06 -0500

    Nested ifs p. 65-66

--- Conditionals.playground/Contents.swift
+++ Conditionals.playground/Contents.swift
@@ -7,7 +7,11 @@ let hasPostOffice: Bool = true
 if population < 10000 {
     message = "\(population) is a small town!"
 } else {
-    message = "\(population) is pretty big!"
+    if population >= 10000 && population < 50000 {
+        message = "\(population) is a medium town!"
+    } else {
+        message = "\(population) is pretty big!"
+    }
 }
 print(message)
 
