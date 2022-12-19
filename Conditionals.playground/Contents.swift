ea3ebc8ea9cd8d6b1deda496b32e0ff1599278dd
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-19 11:36:19 -0500

    else if p. 67-68

--- Conditionals.playground/Contents.swift
+++ Conditionals.playground/Contents.swift
@@ -6,12 +6,10 @@ let hasPostOffice: Bool = true
 
 if population < 10000 {
     message = "\(population) is a small town!"
-} else {
-    if population >= 10000 && population < 50000 {
+} else if population >= 10000 && population < 50000{
         message = "\(population) is a medium town!"
-    } else {
-        message = "\(population) is pretty big!"
-    }
+} else {
+    message = "\(population) is pretty big!"
 }
 print(message)
 
