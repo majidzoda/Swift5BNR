b6a3d8e5abc93ac6d6795b7d046bd83966c05e85
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-20 17:32:14 -0500

    Chapter5: Switch - Tuples and Patern Matching p102-103

--- Switch.playground/Contents.swift
+++ Switch.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-var statusCode: Int = 13
+var statusCode: Int = 418
 var errorString: String = "The request failed with the error: "
 
 switch statusCode {
@@ -14,4 +14,23 @@ case let code where code < 100 || code >= 600:
     errorString = "\(code) is an illegal status code."
 default:
     errorString = "Unexpected error encountered."
+}
+
+let error = (code: statusCode, msg: errorString)
+error.code
+error.msg
+
+let firstErrorCode = 404
+let secondErrorCode = 418
+let errorCodes = (firstErrorCode, secondErrorCode)
+
+switch errorCodes {
+case (404, 404):
+    print("Both error codes were 404.")
+case (404, _):
+    print("Only the 1st code is 404, and we don't care about the 2nd code.")
+case (_, 404):
+    print("Only the 2nd code is 404, and we don't care about the 1st code.")
+default:
+    print("Neither code is 404.")
 }
