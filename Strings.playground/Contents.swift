ef827735c8ecc79586c2df312f957f600c171d53
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-21 15:43:47 -0500

    Chapter7: Strings - Unicode scalars p.132-133

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -9,3 +9,9 @@ print(quote)
 for c: Character in mutablePlayground {
     print("'\(c)'")
 }
+
+let snowman = "\u{2603}"
+let aAcute = "\u{0061}\u{0301}"
+for scalar in playground.unicodeScalars {
+    print("\(scalar.value)")
+}
