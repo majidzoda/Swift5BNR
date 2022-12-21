bb04c1304662f61ab62b7654033f484661b6b56c
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-20 20:37:19 -0500

    Chapter5: Loops - Control Transfers Statements in Loops p.120-124

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -17,3 +17,34 @@ while i < 6 {
     print(myFirstInt)
     i += 1
 }
+
+var shields = 5
+var blastersOverheating = false
+var blasterFireCount = 0
+var spaceDemonsDestroyed = 0
+while shields > 0 {
+    
+    if spaceDemonsDestroyed == 500 {
+        print("You beat the game!")
+        break
+    }
+    
+    if blastersOverheating {
+        print("Blasters overheated! Cooldown initiated.")
+        sleep(5)
+        print("Blasters ready to fire")
+        sleep(1)
+        blastersOverheating = false
+        blasterFireCount = 0
+    }
+    
+    if blasterFireCount > 100 {
+        blastersOverheating = true
+        continue
+    }
+    
+    // Fire blasters!
+    print("Fire blasters!")
+    blasterFireCount += 1
+    spaceDemonsDestroyed += 1
+}
