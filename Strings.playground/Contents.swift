990cc68f25a6e2cc198f1687ac24c5b3c688490e
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-21 15:28:32 -0500

    Chapter7: Strings - Working with Strings p.127-129

---
+++ Strings.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+let playground = "Hello, playground"
+var mutablePlayground = "Hello, mutable playground"
+mutablePlayground += "!"
+let quote = #"I wanted to \"say\":\n\(playground)"#
+print(quote)
