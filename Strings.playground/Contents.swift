f59252fc0c47a8a0d14405f92700c907d8852d27
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-21 16:23:05 -0500

    Chapter7: Strings - Bronze Challenge Complited.

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -30,3 +30,17 @@ let fifthCharacter = playground[end]
 let range = ...end
 let firstFive = playground[range]
 
+/*
+Cbapter7: Bronze Challenge p.140
+Create a new String instance called empty and give it an empty string (a string with no
+characters): let empty = "". It is useful to be able to tell if a string has any characters in it. For example,
+you may be designing a form for data input and want to prevent the user from submitting a blank entry. Use the
+startIndex and endIndex properties on empty to determine whether this string is truly empty.
+*/
+
+let empty = ""
+empty.startIndex == empty.endIndex
+empty.count
+
+
+
