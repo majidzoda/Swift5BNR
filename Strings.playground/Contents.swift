d2f29f3e79da5fa7bffa140c47c2d8013dfdec6f
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-21 15:31:18 -0500

    Chapter7: Strings - Characters p.129 -130

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -5,3 +5,7 @@ var mutablePlayground = "Hello, mutable playground"
 mutablePlayground += "!"
 let quote = #"I wanted to \"say\":\n\(playground)"#
 print(quote)
+
+for c: Character in mutablePlayground {
+    print("'\(c)'")
+}
