543c03afbdf5b7bd0540794c9f3a198e1cc29d93
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-24 17:52:09 -0500

    Chapter9: Optionals - Optional Binding p. 173 - 175

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -6,7 +6,29 @@ import UIKit
 var errorCodeString: String?
 errorCodeString = "404"
 
-if errorCodeString != nil {
-    let theError = errorCodeString!
-    print(theError)
+//if errorCodeString != nil {
+//    let theError = errorCodeString!
+//    print(theError)
+//}
+
+//if let theError = errorCodeString {
+//    print(theError)
+//}
+
+
+
+//if let theError = errorCodeString {
+//    if let errorCodeInt = Int(theError){
+//        print("\(theError): \(errorCodeInt)")
+//    }
+//}
+
+
+
+//if let theError = errorCodeString, let errorCodeInt = Int(theError){
+//    print("\(theError): \(errorCodeInt)")
+//}
+
+if let theError = errorCodeString, let errorCodeInt = Int(theError), errorCodeInt == 404 {
+    print("\(theError): \(errorCodeInt)")
 }
