e27c4c4a5ce5768deff97eddcd7d02880d225d93
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-24 18:21:36 -0500

    Chapter9: Optionals - Bronze Challenge complited p. 186

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -44,5 +44,24 @@ upCaseErrorDescription
 //} else {
 //    description = "No error."
 //}
-errorDescription = nilgi
+errorDescription = nil
 let description = errorDescription ?? "No error."
+
+/*
+Chapter9: Optionals - Bronze Challenge p. 185
+Optionals are best used for things that can literally be nil. That is, they are useful in representing the complete
+absence of something. But nonexistence is not the same as zero. For example, if you are writing code to model a bank
+account and the user has no balance in a given account, the value 0 is more appropriate than nil. The user does not lack
+an account – what they are missing is money! Take a look at the examples below and select which type would model them
+best.
+ - A person’s age: Int or Int?
+ - A person’s middle name: String or String?
+ - A person’s kids’ names: [String] or [String]?
+or [String?]
+*/
+
+var personsAge: Int
+var middleName: String?
+var kidsName: [String]?
+
+
