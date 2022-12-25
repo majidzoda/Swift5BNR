7e380401ce30a3d7231f0cb8fa8876c55fc319b7
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-25 14:12:48 -0500

    Chapter10: Dictionary - Accessing and Modifying Values p. 191

--- Dictionary.playground/Contents.swift
+++ Dictionary.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
 var moveiRatings = ["Tron" : 4, "WarGames" : 5, "Sneakers" : 4]
+moveiRatings.count
+let tronRating = moveiRatings["Tron"]
+
+moveiRatings["Sneakers"] = 5
+moveiRatings
+let oldRating: Int? = moveiRatings.updateValue(5, forKey: "Tron")
+if let lastRating = oldRating, let currentRating = moveiRatings["Tron"] {
+    print("old rating: \(lastRating)")
+    print("current rating: \(currentRating)")
+}
