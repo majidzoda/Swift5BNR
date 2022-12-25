3e8d5557271a5afc05291a11b68be7fcdf732a23
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-25 15:52:30 -0500

    Chapter11: Sets - Getting a Set p. 204 - 205

---
+++ Groceries.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+//var groceryList = Set<String>(["Apples", "Oranges"])
+var groceryList: Set = ["Apples", "Oranges"]
+groceryList.insert("Kiwi")
+groceryList.insert("Pears")
+
+for food in groceryList {
+    print(food)
+}
+
+groceryList.remove("Pears")
