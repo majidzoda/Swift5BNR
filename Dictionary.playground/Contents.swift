c6e56a1549e76f320ac015e27adad30796857d2e
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-25 15:16:00 -0500

    Chapter10: Dictionaries - Silver Challenge complited p.200

--- Dictionary.playground/Contents.swift
+++ Dictionary.playground/Contents.swift
@@ -24,3 +24,46 @@ for movi in moveiRatings.keys {
 }
 
 let watchedMOvies = Array(moveiRatings.keys)
+
+/*
+Chapter 10: Silver Challenge p. 200
+It is not uncommon to place instances of the Array type inside a dictionary. Create a
+dictionary that represents a league of sports teams. Each dictionary key will be the name of an individual
+team (three teams is enough), and each value will be an array of the names of five players on that team. (You
+can make up the team and player names.)
+
+Nesting data structures like this allows you to organize hierarchical data within a single object.
+ 
+In the console, log only the dictionary’s player names. Your result should look something like the output
+below. (We have formatted the array so that the names fit on the page. Your array of names may appear in a
+single long line.)
+ 
+ The NWSL has the following players: ["Jane", "Michaela", "Rachel", "Allysha",
+        "Janine", "Sydney", "Toni", "Shelina", "Emily", "Chioma", "Kailen",
+        "McKenzie", "Thaisa", "Shea", "Jen"]
+*/
+
+var sportsLeague: Dictionary<String,[String]> = ["Alpha" : ["Jane",
+                                                            "Michaela",
+                                                            "Rachel",
+                                                            "Allysha",
+                                                            "Janine"],
+                                                 "Betta" : ["Sydney",
+                                                            "Toni",
+                                                            "Shelina",
+                                                            "Emily",
+                                                            "Chioma"],
+                                                 "Gamma" : ["Kailen",
+                                                            "McKenzie",
+                                                            "Thaisa",
+                                                            "Shea",
+                                                            "Jen"]
+]
+
+var printArray: [String] = []
+for values in sportsLeague.values{
+    printArray += values
+}
+
+print(" The NWSL has the following players: \(printArray)")
+
