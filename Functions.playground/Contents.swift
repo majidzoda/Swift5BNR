33d2f8fab2fa4417a25f23320dfa0efa87e90005
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-26 13:02:06 -0500

    Chapter12: Functions - Silver Challenge complited p. 240

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -93,3 +93,40 @@ func greetByMiddleNameBronzeChallenge (fromFullName name: (first: String, middle
     print("Hey \(name.first) \(middleName[middleName.startIndex]). \(name.last)")
 }
 greetByMiddleNameBronzeChallenge(fromFullName: ("Alois", "Rumpelstiltskin", "Chaz"))
+
+
+/*
+Chapter 12: Silver Challenge p. 240
+Write a function called siftBeans(fromGroceryList:) that takes a grocery list (as an array of strings) and “sifts out” the
+beans from the other groceries. The function should take one argument that has a parameter name called list, and it should
+return a named tuple of the type (beans: [String], otherGroceries: [String]). Here is an example of how you should be able to
+call your function and what the result should be:
+ 
+let result = siftBeans(fromGroceryList: ["green beans",
+                                             "milk",
+                                             "black beans",
+                                             "pinto beans",
+                                             "apples"])
+
+result.beans == ["green beans", "black beans", "pinto beans"] // true
+result.otherGroceries == ["milk", "apples"] // true
+ 
+Hint: You may need to use a function on the String type called hasSuffix(_:).
+*/
+
+func siftBeans (fromGroceryList list:[String]) -> (beans: [String], otherGroceries: [String]) {
+    var beans = [String](), otherItems = [String]()
+    for item in list {
+        if item.hasSuffix("beans") {
+            beans.append(item)
+        } else { otherItems.append(item) }
+    }
+    return (beans, otherItems)
+}
+let result = siftBeans(fromGroceryList: ["green beans",
+                                         "milk",
+                                         "black beans",
+                                         "pinto beans",
+                                         "apples"])
+result.beans == ["green beans", "black beans", "pinto beans"]
+result.otherGroceries == ["milk", "apples"]
