9b445f749ce77d9acaf54b1e76ed1f99cac9b862
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-26 11:16:28 -0500

    Chapter12: Functions - Function Parameters  p. 220

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -4,3 +4,13 @@ func printGreetings() {
     print("Hello, playground.")
 }
 printGreetings()
+
+func printPersonalGreeting(name: String) {
+    print("Hello, \(name). Welcome to your playground.")
+}
+printPersonalGreeting(name: "step")
+
+func divisionDescriptionFor (numerator: Double, denominator: Double) {
+    print("\(numerator) divided by \(denominator) is \(numerator / denominator)")
+}
+divisionDescriptionFor(numerator: 9.0, denominator: 3.0)
