c85189e47a2942fc27e50bf6db2ebbaeb2890217
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-26 12:14:48 -0500

    Chapter12: Functions - Multiple Returns p. 231

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -36,3 +36,20 @@ func areaOfTriangleWith(base: Double, height: Double) -> Double {
     return divide()
 }
 print(areaOfTriangleWith(base: 3.0, height: 5.0))
+
+func sortedEvenOddNumbers(_ numbers: [Int]) -> (evens: [Int], odds: [Int]) {
+    var evens = [Int]()
+    var odds = [Int]()
+    for number in numbers {
+        if number % 2 == 0 {
+            evens.append(number)
+        } else {
+            odds.append(number)
+        }
+    }
+    return (evens, odds)
+}
+
+let aBunchOfNumbers = [10,1,4,3,57,43,84,27,156,111]
+let theSortedNumbers = sortedEvenOddNumbers(aBunchOfNumbers)
+print("The even numbers are: \(theSortedNumbers.evens) the odd numbers are: \(theSortedNumbers.odds)")
