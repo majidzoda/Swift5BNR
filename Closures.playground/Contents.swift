d8e7d6f518c952a752a428b6777a5046e2b9ea05
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-26 13:52:27 -0500

    Chapter13: Closures - Closure Expression Syntax p. 249 - 252

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -2,8 +2,17 @@ import Cocoa
 
 let volunteerCounts = [1, 3, 40, 32, 2, 53, 77, 13]
 
-func isAcending (_ i: Int, _ j: Int) -> Bool {
-    return i < j
-}
-let volunteersSorted = volunteerCounts.sorted(by: isAcending(_:_:))
+//func isAcending (_ i: Int, _ j: Int) -> Bool {
+//    return i < j
+//}
+//let volunteersSorted = volunteerCounts.sorted(by: isAcending(_:_:))
+
+//let volunteersSorted = volunteerCounts.sorted(by: {
+//    (i: Int, j: Int) -> Bool in
+//    return i < j
+//})
+
+//let volunteersSorted = volunteerCounts.sorted(by: { i, j in i < j})
+//let volunteersSorted = volunteerCounts.sorted(by: { $0 < $1 })
+let volunteersSorted = volunteerCounts.sorted { $0 < $1 }
 print(volunteersSorted)
