9322726a404dc7d5d9f10b2e744d446db40a0410
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-26 14:13:43 -0500

    Chapter13: Closures - Functions as Arguments p. 253 - 255

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -16,3 +16,20 @@ let volunteerCounts = [1, 3, 40, 32, 2, 53, 77, 13]
 //let volunteersSorted = volunteerCounts.sorted(by: { $0 < $1 })
 let volunteersSorted = volunteerCounts.sorted { $0 < $1 }
 print(volunteersSorted)
+
+func format (numbers: [Double], using formatter: (Double) -> String = { "\($0)" }) -> [String] {
+    var result = [String]()
+    for number in numbers {
+        let transformed = formatter(number)
+        result.append(transformed)
+    }
+    return result
+}
+
+let rounder: (Double) -> String = {
+    (num: Double) -> String in
+    return "\(Int(num.rounded()))"
+}
+let volunteerAverages = [10.75, 4.2, 1.5, 12.12, 16.815]
+let roundedAverageAsStrings = format(numbers: volunteerAverages, using: rounder)
+let exactAveragesAsStrings = format(numbers: volunteerAverages)
