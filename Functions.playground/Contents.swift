17694fb3be4c678748262aff6a9af475a393ab43
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-26 12:52:39 -0500

    Chapter12: Functions - Bronze Challenge complited p. 239

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -74,3 +74,22 @@ greetByMiddleName(fromFullName: ("Alice", "Richards", "Ward"))
 
 let evenOddFunction: ([Int]) -> ([Int], [Int]) = sortedEvenOddNumbers(_:)
 evenOddFunction([1,2,3])
+
+/*
+Chapter12: Functions - Bronze Challenge p. 239
+Like if/else conditions, guard statements support the use of multiple clauses to perform additional checks. Using additional
+clauses with a guard statement gives you further control over the statement’s condition. Refactor the
+greetByMiddleName(fromFullName:) function to have an additional clause in its guard statement. This clause should check
+whether the middle name is longer than 10 characters. If it is, then greet the person with their first name, their middle
+initial (the first letter of the middle name followed by a period), and their last name instead. For example, if the name is
+Alois Rumpelstiltskin Chaz, the function should print Hey, Alois R. Chaz.
+*/
+
+func greetByMiddleNameBronzeChallenge (fromFullName name: (first: String, middle: String?, last: String)) {
+    guard let middleName = name.middle, middleName.count > 10 else {
+        print("Hey there!")
+        return
+    }
+    print("Hey \(name.first) \(middleName[middleName.startIndex]). \(name.last)")
+}
+greetByMiddleNameBronzeChallenge(fromFullName: ("Alois", "Rumpelstiltskin", "Chaz"))
