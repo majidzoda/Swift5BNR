3c16ab1ce8ab29009c5a18a81de7c252e4236d0b
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-29 14:26:59 -0500

    Chapter14: Enumerations - Enumerations with Raw Values p. 280 - 282

--- Enumerations.playground/Contents.swift
+++ Enumerations.playground/Contents.swift
@@ -1,13 +1,31 @@
 import Cocoa
 
-enum TextAlignment {
-    case left
-    case right
-    case center
-    case justify
+enum TextAlignment: Int {
+    case left       = 20
+    case right      = 30
+    case center     = 40
+    case justify    = 50
 }
 
 var alignemt = TextAlignment.justify
+
+TextAlignment.left.rawValue
+TextAlignment.right.rawValue
+TextAlignment.center.rawValue
+TextAlignment.justify.rawValue
+alignemt.rawValue
+
+// Create a raw value
+let myRawValue = 20
+
+// Try to convert the raw value into a TextAlignment
+if let myAlignment = TextAlignment(rawValue: myRawValue) {
+    // Conversion succeeded!
+    print("successfully created \(myAlignment) from \(myRawValue)")
+} else {
+    // Conversion failed
+    print("\(myRawValue) has no coresponding TextAliment case")
+}
 
 //if alignemt == .right {
 //    print("We should right-align the text")
@@ -23,3 +41,14 @@ case .center:
 case .justify:
     print("justified")
 }
+
+enum ProgrammingLanguage: String {
+    case swift
+    case objectiveC = "objective-c"
+    case c
+    case cpp        = "c++"
+    case java
+}
+
+let myFavoriteLanguage = ProgrammingLanguage.swift
+print("My favorite programming language is \(myFavoriteLanguage.rawValue)")
