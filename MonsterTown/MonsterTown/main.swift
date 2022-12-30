9ba43227e0d0e42029e8c18dd47297038fc7009f
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-30 14:34:09 -0500

    Chapter15: Struct And Clases - Poymorphism and type casting p. 321 - 325

--- MonsterTown/MonsterTown/main.swift
+++ MonsterTown/MonsterTown/main.swift
@@ -2,13 +2,9 @@ import Foundation
 
 var myTown = Town()
 myTown.changePopulation(by: 500)
-//myTown.printDescription()
 
-//let genericMonster = Monster()
-//genericMonster.town = myTown
-//genericMonster.terrorizeTown()
-
-let fredTheZombie = Zombie()
+let fredTheZombie: Monster = Zombie()
 fredTheZombie.town = myTown
 fredTheZombie.terrorizeTown()
 fredTheZombie.town?.printDescription()
+(fredTheZombie as? Zombie)?.walksWithLamp = true
