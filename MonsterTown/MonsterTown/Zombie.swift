1b593f87be3c05013fda9f40cc81f1f81477e594
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-30 14:52:40 -0500

    Chapter15: Struct And Clases - Looking Ahead: What is the Real Difference? p. 326 - 328

--- MonsterTown/MonsterTown/Zombie.swift
+++ MonsterTown/MonsterTown/Zombie.swift
@@ -3,8 +3,13 @@ import Foundation
 class Zombie: Monster {
     var walksWithLamp = true
     
+    func regenerate() {
+        walksWithLamp = false
+    }
+    
     override func terrorizeTown() {
         town?.changePopulation(by: -10)
         super.terrorizeTown()
+        regenerate()
     }
 }
--- MonsterTown/MonsterTown/main.swift
+++ MonsterTown/MonsterTown/main.swift
@@ -1,10 +1,15 @@
 import Foundation
 
 var myTown = Town()
+var yourTown = myTown // Now there are two towns!
 myTown.changePopulation(by: 500)
+print("myTown has \(myTown.population) population and yourTown has \(yourTown.population) population")
 
-let fredTheZombie: Monster = Zombie()
+let fredTheZombie = Zombie()
 fredTheZombie.town = myTown
 fredTheZombie.terrorizeTown()
 fredTheZombie.town?.printDescription()
-(fredTheZombie as? Zombie)?.walksWithLamp = true
+
+var frederickTheZombie = fredTheZombie // Still only one zombie!
+frederickTheZombie.name = "Frederick"
+print("Fred's name is \(fredTheZombie.name)")
