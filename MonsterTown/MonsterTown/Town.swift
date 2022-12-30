c266506068f83b344708edbed03e196322de3264
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-30 13:42:17 -0500

    Chapter15: Struct And Clases - mutatung methods p. 311 - 312

--- MonsterTown/MonsterTown/Town.swift
+++ MonsterTown/MonsterTown/Town.swift
@@ -7,4 +7,8 @@ struct Town {
     func printDescription () {
         print("Population: \(population); number of stopLights: \(numberOfStopLights)")
     }
+    
+    mutating func changePopulation (by amount: Int) {
+        population += amount
+    }
 }
--- MonsterTown/MonsterTown/main.swift
+++ MonsterTown/MonsterTown/main.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 var myTown = Town()
+myTown.changePopulation(by: 500)
 myTown.printDescription()
-
