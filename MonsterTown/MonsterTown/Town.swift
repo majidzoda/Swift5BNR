8c1edab0a1e4b03aa6ddf9b81fc873c8d4548cc8
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-30 13:31:39 -0500

    Chapter15: Struct And Clases - Structures p. 307 - 310

---
+++ MonsterTown/MonsterTown/Town.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct Town {
+    var population = 5_422
+    var numberOfStopLights = 4
+}
---
+++ MonsterTown/MonsterTown/main.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+var myTown = Town()
+print("Population: \(myTown.population); number of stopLights: \(myTown.numberOfStopLights)")
+
