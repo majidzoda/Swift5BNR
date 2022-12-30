8ef21752a6ff91858a93d205479aa132f0c41ea5
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-30 13:51:11 -0500

    Chapter15: Struct And Clases - Classes p. 314 - 315

--- MonsterTown/MonsterTown/main.swift
+++ MonsterTown/MonsterTown/main.swift
@@ -3,3 +3,7 @@ import Foundation
 var myTown = Town()
 myTown.changePopulation(by: 500)
 myTown.printDescription()
+
+let genericMonster = Monster()
+genericMonster.town = myTown
+genericMonster.terrorizeTown()
