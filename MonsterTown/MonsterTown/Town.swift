609a4f47f47de33de8b05c3b067ffc188dbfe5af
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2022-12-31 15:46:04 -0500

    Chapter16: Properties - Computed  Properties p. 342

--- MonsterTown/MonsterTown/Town.swift
+++ MonsterTown/MonsterTown/Town.swift
@@ -11,7 +11,18 @@ struct Town {
         case large
     }
     
-    lazy var townSize: Size = {
+//    lazy var townSize: Size = {
+//        switch population {
+//        case 0...10_000:
+//            return Size.small
+//        case 10_001...100_000:
+//            return Size.medium
+//        default:
+//            return Size.large
+//        }
+//    }()
+    
+    var townSize: Size {
         switch population {
         case 0...10_000:
             return Size.small
@@ -20,8 +31,7 @@ struct Town {
         default:
             return Size.large
         }
-    }()
-    
+    }
     
     func printDescription () {
         print("Population: \(population); number of stopLights: \(numberOfStopLights)")
