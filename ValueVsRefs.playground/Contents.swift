2013efb9a2aacc29903dee8f2f34eb08b14e2829
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-02 15:17:28 -0500

    Chapter18: Value vs Reference Types - Reference Semantics p.403 - 405

--- ValueVsRefs.playground/Contents.swift
+++ ValueVsRefs.playground/Contents.swift
@@ -4,3 +4,13 @@ var str = "Hello, playground"
 var playgroundGreeting = str
 playgroundGreeting += "! How are you today?"
 str
+
+class Employee {
+    var id: Int = 0
+}
+let anika = Employee()
+let theBoss = anika
+
+anika.id = 16
+anika.id
+theBoss.id
