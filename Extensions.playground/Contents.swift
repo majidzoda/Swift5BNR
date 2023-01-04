fde59c110c73a740f34cbc093dbc72a7b4ef50da
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-04 13:10:41 -0500

    Chapter20: Extensions - Extending and existing type & your own type p.463 - 465

---
+++ Extensions.playground/Contents.swift
@@ -0,0 +1,27 @@
+import Cocoa
+
+extension Double {
+    var squared: Double { return self * self }
+}
+
+let sideLength: Double = 12.5
+let area = sideLength.squared
+
+struct Car {
+    let make: String
+    let model: String
+    let year: Int
+    var fuelLevel: Double {
+        willSet {
+            precondition(newValue <= 1.0 && newValue >= 0.0, "New value must be between 0 and 1.")
+        }
+    }
+}
+
+let firstCar = Car(make: "Benz", model: "Patent Motorwagon", year: 1886, fuelLevel: 0.5)
+
+extension Car: CustomStringConvertible {
+    var description: String {
+        return "\(year) \(make) \(model), fuel level: \(fuelLevel)"
+    }
+}
