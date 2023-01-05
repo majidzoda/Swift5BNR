345cbcc73687fe89b547ba3fcbb618fd9c7d53d6
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-05 16:13:00 -0500

    Chapter21: Generics - Type Constrains p. 482 - 483

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -25,12 +25,12 @@ intStack.push(1)
 intStack.push(2)
 var doubleStack = intStack.map { 2 * $0 }
 
-print(String(describing: intStack.pop()))
-print(String(describing: intStack.pop()))
-print(String(describing: intStack.pop()))
-
-print(String(describing: doubleStack.pop()))
-print(String(describing: doubleStack.pop()))
+//print(String(describing: intStack.pop()))
+//print(String(describing: intStack.pop()))
+//print(String(describing: intStack.pop()))
+//
+//print(String(describing: doubleStack.pop()))
+//print(String(describing: doubleStack.pop()))
 
 var stringStack = Stack<String>()
 stringStack.push("this is a string")
@@ -50,3 +50,19 @@ func myMap<T,U>(_ items: [T], _ txform: (T) -> (U)) -> [U] {
 let strings = ["one", "two", "three"]
 let stringLength = myMap(strings, { $0.count})
 print(stringLength)
+
+func checkIfEqual<T: Equatable>(_ first: T, second: T) -> Bool {
+    return first == second
+}
+
+print(checkIfEqual(1, second: 1))
+print(checkIfEqual("a string", second: "a string"))
+print(checkIfEqual("a string", second: " a different string"))
+
+func checkIfDescriptionMatch<T: CustomStringConvertible, U: CustomStringConvertible>(_ first: T, _ second: U) -> Bool {
+    return first.description == second.description
+}
+
+print(checkIfDescriptionMatch(Int(1),  UInt(1)))
+print(checkIfDescriptionMatch(1, 1.0))
+print(checkIfDescriptionMatch(Float(1.0), Double(1.0)))
