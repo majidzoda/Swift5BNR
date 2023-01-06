5f51a8fac86e2043f30b32d6710a895e5c5df32c
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-06 18:00:50 -0500

    Chapter22: ProtocolExtensions - Silver Challenge complited p. 522

--- ProtocolExtensions.playground/Contents.swift
+++ ProtocolExtensions.playground/Contents.swift
@@ -1,4 +1,5 @@
 import Cocoa
+import Foundation
 
 protocol Exercise: CustomStringConvertible {
     var caloriesBurned: Double { get set }
@@ -81,3 +82,57 @@ let workout: Exercise = tenKRun
 
 print(tenKRun.title)
 print(workout.title)
+
+/*
+Chapter22: Protocol Extensions - Bronze Challenge p. 521
+Add a method called count(where:) to all collections to count the number of elements that pass a provided test.
+
+You should be able to call it like this:
+    let workouts: [Exercise] = [ellipticalWorkout, runningWorkout, tenKRun]
+    let hardWorkoutCount = workouts.count(where: { $0.caloriesBurned >= 500})   // 1
+
+Look at the documentation for the Sequence and Collection protocols. Why is Collection the better protocol to add this extension to?
+*/
+
+extension Collection where Element == Exercise {
+    func count(wh: ((Exercise) -> Bool)) -> Int {
+        var count = 0
+        for exercise in self {
+            if wh(exercise) {
+                count += 1
+            }
+        }
+        return count
+    }
+}
+let workouts: [Exercise] = [ellipticalWorkout, runningWorkout, tenKRun]
+let hardWorkoutCount = workouts.count(wh: {$0.caloriesBurned >= 500 })  // 1
+print(hardWorkoutCount)
+
+/*
+Chapter22: Protocol Extension - Silver Challenge p. 522
+Sometimes you have a sequence of integers and want to add them up. You can do that by calling reduce(_:_:), but that is ugly and
+counterintuitive. (Feel free to take time to review the reduce(_:_:) method in Chapter 13 on closures.)
+ 
+To make summing sequences of number more convenient, add a computed property called sum to all sequences of numbers. You should be able to use
+it like this:
+    [4, 8, 15, 16, 23, 42].sum       // 108
+    [80.5, 9.6].sum                  // 90.1
+ 
+To solve this challenge, you will need to be able to answer a deeper question: What is a number? Look at the documentation for the Int and
+Double types. What protocols do they conform to? What protocols do those protocols inherit from? Do Int and Double have any protocols in common
+that would be a good place to add your extension?
+*/
+
+extension Collection where Element: Numeric {
+    var sum: Element {
+        var total: Element = 0
+        for element in self {
+            total += element
+        }
+        return total
+    }
+}
+
+[4, 8, 15, 16, 23, 42].sum
+[80.5, 9.6].sum 
