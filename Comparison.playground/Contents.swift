9eee7fa03fe1421401545bef6105f4354e58fded
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-02-15 13:33:52 -0500

    Chapter25-EquatableComparableHashable - Equatable p. 638  - 641

---
+++ Comparison.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+struct Point {
+    let x: Int
+    let y: Int
+    let label: String? = nil
+    
+
+}
+
+extension Point: Equatable {
+    static func ==(lhs: Point, rhs: Point) -> Bool {
+        return (lhs.x == rhs.x) && (lhs.y == rhs.y)
+    }
+}
+
+let a  = Point(x: 3, y: 4)
+let b  = Point(x: 3, y: 4)
+let abEqual = (a == b)
