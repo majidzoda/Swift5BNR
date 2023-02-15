2ba278aadbf0833e44808af69ce4654b21f06ffa
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-02-15 14:03:51 -0500

    Chapter25-EquatableComparableHashable - Comparable p. 644 - 646

--- Comparison.playground/Contents.swift
+++ Comparison.playground/Contents.swift
@@ -8,9 +8,13 @@ struct Point {
 
 }
 
-extension Point: Equatable {
+extension Point: Equatable, Comparable {
     static func ==(lhs: Point, rhs: Point) -> Bool {
         return (lhs.x == rhs.x) && (lhs.y == rhs.y)
+    }
+    
+    static func < (lhs: Point, rhs: Point) -> Bool {
+        return (lhs.x < rhs.x) && (lhs.y < rhs.y)
     }
 }
 
@@ -18,3 +22,17 @@ let a  = Point(x: 3, y: 4)
 let b  = Point(x: 3, y: 4)
 let abEqual = (a == b)
 let abNotEqua = (a != b)
+
+let c = Point(x: 2, y: 6)
+let d = Point(x: 3, y: 7)
+let cdEqual = (c == d)
+let cLessThanD = (c < d)
+
+let cLessThanEqualD = ( c <= d)
+let cGreaterThanD = (c > d)
+let cGreaterThanEqualD = (c >= d)
+
+let pointRange = c..<d
+pointRange.contains(a)
+pointRange.contains(Point(x: -1, y: -1))
+
