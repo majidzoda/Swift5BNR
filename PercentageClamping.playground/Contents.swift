aafde8d29d863d2e16e0eacc1335eb00647c17e0
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-02-17 14:10:06 -0500

    Chapter26-PropertyWrappers - Additional Configuration p. 668 - 670

--- PercentageClamping.playground/Contents.swift
+++ PercentageClamping.playground/Contents.swift
@@ -3,8 +3,11 @@ import Cocoa
 struct Car {
     @Percentge var fuelLevel: Double = 1.0
     @Percentge var wiperFluidLevel: Double = 0.5
+    @Percentge(upperBound: 2.0) var stereoVolume: Double = 1.0
 }
 
 var myCar = Car()
 myCar.fuelLevel = 1.1
 print("Fuel:", myCar.fuelLevel)
+myCar.stereoVolume = 2.5
+print("Volume:", myCar.stereoVolume)
--- PercentageClamping.playground/Sources/Percentage.swift
+++ PercentageClamping.playground/Sources/Percentage.swift
@@ -2,14 +2,16 @@ import Foundation
 
 @propertyWrapper public struct Percentge {
     private var storage: Double
+    private var upperBound: Double
     
-    public init (wrappedValue: Double){
+    public init (wrappedValue: Double, upperBound: Double = 1){
         storage = max(min(wrappedValue, 1), 0)
+        self.upperBound = upperBound
     }
     
     public var wrappedValue: Double {
         set {
-            storage = max(min(newValue, 1), 0)
+            storage = max(min(newValue, upperBound), 0)
         }
         get {
             return storage
